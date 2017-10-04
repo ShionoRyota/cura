91d8d9802f38c9500d21abe0a85f8832a82b2de6
塩野涼太 <ryota@shionoryoutanoMacBook-Air.local>
2017-10-04 18:09:41 +0900

    helpersのpageからログアウトできるようにapplicationcontrollerにコード書き足しました。 helpersのpageのnavber書き換えました。

--- app/controllers/helpers_controller.rb
+++ app/controllers/helpers_controller.rb
@@ -1,2 +1,5 @@
 class HelpersController < ApplicationController
+	def index
+		@helpers = Helper.all
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :personals
   devise_for :workers
 
+  resources :helpers
+  get 'homes/top'
 end
